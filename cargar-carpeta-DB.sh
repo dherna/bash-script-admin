932277814c611a74a35e2263ed1d5daa46060597
Diego Hernandez <diego.herna@gmail.com>
2014-06-03 17:34:10 +0200

    Update cargar-carpeta-DB.sh
    Probado y resuelto varios errores.

--- cargar-carpeta-DB.sh
+++ cargar-carpeta-DB.sh
@@ -10,31 +10,37 @@ MYSQLPASS='estaesmicontraseña'
 MYSQLHOST="localhost"
 
 # LISTADO DE CARPETA TEMPORAL Y VARIABLILIZARLO
-DBS="echo `ls`"
-
+DBS="`ls`"
 
 for db in $DBS
 do
         echo "Descomprimiendo $db"
         gzip -d $db
-        if [ "$?" -ne "0" ]; then 
+        if [ "$?" -ne "0" ]; then
                  echo "########## Error en Descompresióni $db - Date: $(date) "
-        else
-                NAMEDB="${db#*.es_mysql_}"
-                echo "$NAMEDB"
-                NAMEDBF="${NAMEDB%.gz*}"
-                echo "Creando MySQL moodle1_$NAMEDBF"
-                mysql -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS -e 'DROP DATABASE moodle1_'$NAMEDBF';'
-                echo "CREATE DATABASE  $NAMEDBF"
-                mysql -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS -e 'CREATE DATABASE moodle1_'$NAMEDBF';'
+        else   
+                FILE="${db#*.es_mysql_}"
+                echo "sin .es_mysql_ $FILE"
+                NAMEDB="${FILE%.gz*}"
+                echo "sin .es_mysql_ ni .gz $NAMEDB"
+                NAMEFILE="${db%.gz*}"
+                echo "sin .gz añadinedo  moodle1_ - $NAMEDB"
+                mysql -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS -e 'DROP DATABASE moodle1_'$NAMEDB';'
+                echo "CREATE DATABASE  $NAMEDB"
+                mysql -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS -e 'CREATE DATABASE moodle1_'$NAMEDB';'
                 if [ "$?" -ne "0" ]; then
-                        echo "########## Error Al crear la Base de Datos moodle1_'$NAMEDBF - Date: $(date) "
-                else
-                        echo "SOURCE $db"
-                        mysql -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS moodle1_$NAMEDBF -e 'SOURCE '$db';'
+                        echo "########## Error Al crear la Base de Datos moodle1_'$NAMEDB - Date: $(date) "
+                else   
+                        echo "SOURCE $NAMEFILE"
+                        echo "mysql -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS moodle1_$NAMEDB -e 'SOURCE '$NAMEFILE';'"
+                        mysql -u $MYSQLUSER -h $MYSQLHOST -p$MYSQLPASS moodle1_$NAMEDB -e 'SOURCE '$NAMEFILE';'
                         if [ "$?" -ne "0" ]; then
                                 echo "########## Error al cargar la Base de Datos moodle1_'$NAMEDBF - Date: $(date) "
-                        else
+                        else   
                                 echo "Termino de cargar, borrando fichero,  pasamos al siguiente."
                                 rm $db
+                        fi
+                fi
+        fi
+
 done
